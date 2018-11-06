5ebee795defc18d94910f1b382cbb83d1d9dde40
Adrian Smith <adrian.smith@keysight.com>
2018-11-06 10:32:59 -0800

    added movement/placement logic

--- grid/grid.go
+++ grid/grid.go
@@ -1,14 +1,31 @@
 package grid
 
 import (
+	"fmt"
+
 	"github.com/VivaLaPanda/antipath/entity"
 	"github.com/VivaLaPanda/antipath/grid/tile"
+	uuid "github.com/satori/go.uuid"
 )
 
 type State struct {
 	grid     [][]tile.Tile
-	entities map[string]entity.Entity
+	size     int
+	entities map[string]Coordinates
 }
+
+type Coordinates struct {
+	X, Y int
+}
+
+type Direction int
+
+const (
+	Up    Direction = iota
+	Right Direction = iota
+	Left  Direction = iota
+	Down  Direction = iota
+)
 
 func NewState(size int) (grid *State) {
 	gridData := make([][]tile.Tile, size)
@@ -17,6 +34,90 @@ func NewState(size int) (grid *State) {
 	}
 	return &State{
 		grid:     gridData,
-		entities: make(map[string]entity.Entity),
+		size:     size, // faster than using len every time
+		entities: make(map[string]Coordinates),
 	}
+}
+
+func (s *State) GetTile(pos Coordinates) (*tile.Tile, error) {
+	if outOfBounds(s.size, pos) {
+		return nil, fmt.Errorf("provided pos is out of bounds. Pos: %v, maxsize: %d", pos, s.size)
+	}
+	return &s.grid[pos.X][pos.Y], nil
+}
+
+func (s *State) NewEntity(data entity.Entity, pos Coordinates) (id string, err error) {
+	targetTile, err := s.GetTile(pos)
+	if err != nil {
+		return "", err
+	}
+
+	if err := targetTile.SetEntity(data); err != nil {
+		return "", fmt.Errorf("provided pos can't contain an entity, already full. Tile %v", targetTile)
+	}
+
+	id = uuid.Must(uuid.NewV4()).String()
+
+	s.entities[id] = pos
+
+	return id, nil
+}
+
+func (s *State) Move(entityID string, dir Direction, speed int, instant bool) (err error) {
+	// Get the location of the entity
+	sourcePos, exists := s.entities[entityID]
+	if !exists {
+		return fmt.Errorf("provided entity ID not valid. ID: %s", entityID)
+	}
+	// Get the tile data at that location
+	sourceTile, err := s.GetTile(sourcePos)
+	if err != nil {
+		return fmt.Errorf("couldn't get tile at provided pos, pos: %v, err: %s", sourcePos, err)
+	}
+
+	// Calculate the movement accounting for instant
+	var targetPos Coordinates
+	var targetTile *tile.Tile
+	for targetPos = sourcePos; speed > 0; speed -= 1 {
+		posDelta := 1
+		if instant {
+			posDelta = speed
+		}
+		switch dir {
+		case Up:
+			targetPos.Y += posDelta
+		case Down:
+			targetPos.Y -= posDelta
+		case Left:
+			targetPos.X -= posDelta
+		case Right:
+			targetPos.X += posDelta
+		}
+
+		// Get tile data for where we moved to
+		targetTile, err = s.GetTile(targetPos)
+		if err != nil {
+			return fmt.Errorf("couldn't move to resulting pos: %v, err: %s", targetPos, err)
+		}
+
+		// Make sure out target is free
+		if !targetTile.IsFree() {
+			return fmt.Errorf("couldn't move to resulting pos: %v, tile is occupied", targetPos)
+		}
+
+		if instant {
+			break
+		}
+	}
+
+	// Move the entity
+	entityData := sourceTile.PopEntity()
+	targetTile.SetEntity(entityData)
+	s.entities[entityID] = targetPos
+
+	return nil
+}
+
+func outOfBounds(size int, pos Coordinates) bool {
+	return pos.X > size || pos.Y > size
 }
--- grid/tile/tile.go
+++ grid/tile/tile.go
@@ -1,8 +1,42 @@
 package tile
 
+import (
+	"fmt"
+
+	"github.com/VivaLaPanda/antipath/entity"
+)
+
 type Tile struct {
 	alignment int
+	entity    entity.Entity
 	//hazard *Hazard
-	totem          bool
+	totemHealth    int
 	alignmentDelta int
+}
+
+func (tile *Tile) IsFree() bool {
+	if tile.totemHealth > 0 {
+		return false
+	}
+	if tile.entity != nil {
+		return false
+	}
+
+	return true
+}
+
+func (tile *Tile) SetEntity(entity entity.Entity) error {
+	if tile.entity != nil {
+		return fmt.Errorf("can only SetEntity if entity is already nil, remove before setting")
+	}
+	tile.entity = entity
+
+	return nil
+}
+
+func (tile *Tile) PopEntity() entity.Entity {
+	var ref entity.Entity // declare so the pointer logic is a little clearer
+	ref = tile.entity
+	tile.entity = nil
+	return &ref
 }
